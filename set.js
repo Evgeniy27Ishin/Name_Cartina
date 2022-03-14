86358c6c9a8963ece028586b3ee48e59ec2bafcd
Evgeniy <evgeniy.ishin01@gmail.com>
2022-03-14 16:23:18 +0700

    zh1

--- set.js
+++ set.js
@@ -1,17 +1,52 @@
-$(document).ready(function(){
+let students = []
+loadFromSite()
+//получаем данные с сервера
+function loadFromSite() {
+	$.get('http://217.71.129.139:4035/students.php', function(data){
+		students = JSON.parse(data)['response'];
+	});
+}
 
-    $("#el1").draggable({stack:"#el1"}); 
-    $("#el1").resizable();
-    $("#sort1").sortable();
-    $("#el2,#el3").draggable({stack:"#el2,el3"});
-    $("#drop1,#drop2,#el3");
-    $("#drop1").html("Евгений");
-    $("#drop2").html("Ишин")({
 
-	over:function(){
-         $("#warp1").css("background-color","#d7fa99");
-         $("#drop1").css("background-color","#d7fa99");
-      }
-  	});
+function loadStudenst() {
 
-}); 
+	let table = $('#tbl_all')
+
+	for (let i = 0; i < students.length; i++) {
+		//создадим новую строку
+		let tr = $('<tr>')
+		//создадим 3 ячейки
+		let td1 = $('<td>' + students[i].id + '</td>')
+		let td2 = $('<td>' + students[i].name + '</td>')
+		let td3 = $('<td>' + students[i].surname + '</td>')
+		let td4 = $('<td><button onclick="show_info('+i+')">Подробно</button></td>')
+		
+		//прикрепим ячейки к строке
+		tr.append(td1).append(td2).append(td3).append(td4)
+		//прикрепим строку к таблице
+		table.append(tr)
+	}
+		
+	$('#btn').disabled = true
+}
+
+function show_info(id) {
+	//находим блок
+	let div = $('#info')
+
+	//создадим заголовок
+	let header = $('<h1>Информация о студенте № '+id+'<h1>')
+
+	//вывод имени и фамилии
+	let name = $('<h3>'+students[id].name+' '+students[id].surname+'<h3>')
+	 
+
+	//вывод логотипа
+	let img = $('<img src="http://217.71.129.139:4035/' + students[id].logo+'">')
+
+	//очистим блок
+	div.html('')
+	//добавим элементы к блоку
+	div.append(header).append(name).append(img)
+}
+
