e079363b578271a4fee15f50c5da1a27b26bd04c
Evgeniy <evgeniy.ishin01@gmail.com>
2022-03-14 16:25:59 +0700

    zh2

--- set.js
+++ set.js
@@ -1,52 +1,117 @@
-let students = []
-loadFromSite()
-//получаем данные с сервера
-function loadFromSite() {
-	$.get('http://217.71.129.139:4035/students.php', function(data){
-		students = JSON.parse(data)['response'];
-	});
+function draw() {
+         let canvas = document.getElementById('drawRound');
+         let ctx = canvas.getContext('2d');  
+         let pi = Math.PI;
+  
+
+//1
+ctx.beginPath();
+ctx.lineWidth = 7;
+ctx.strokeStyle = "green";
+ctx.fillStyle="blue";
+ctx.arc(40, 160, 20, 0, 2*pi, false);
+ctx.stroke();
+ctx.fill(); 
+
+//2
+ctx.beginPath();
+ctx.lineWidth = 7;
+ctx.strokeStyle = "green";
+ctx.fillStyle="blue";
+ctx.arc(165, 150, 20, 0, 2*pi, false);
+ctx.stroke();
+ctx.fill(); 
+
+//3
+ctx.beginPath();
+ctx.lineWidth = 7;
+ctx.strokeStyle = "green";
+ctx.fillStyle="blue";
+ctx.arc(145, 100, 20, 0, 2*pi, false);
+ctx.stroke();
+ctx.fill(); 
+
+//4
+ctx.beginPath();
+ctx.lineWidth = 7;
+ctx.strokeStyle = "green";
+ctx.fillStyle="blue";
+ctx.arc(135, 200, 20, 0, 2*pi, false);
+ctx.stroke();
+ctx.fill(); 
+
+//5
+ctx.beginPath();
+ctx.lineWidth = 7;
+ctx.strokeStyle = "green";
+ctx.fillStyle="blue";
+ctx.arc(45, 110, 20, 0, 2*pi, false);
+ctx.stroke();
+ctx.fill(); 
+
+//6
+ctx.beginPath();
+ctx.lineWidth = 7;
+ctx.strokeStyle = "green";
+ctx.fillStyle="blue";
+ctx.arc(95, 80, 20, 0, 2*pi, false);
+ctx.stroke();
+ctx.fill(); 
+
+
+
+
+//Окружности
+ ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.strokeStyle = "green";
+ctx.fillStyle="yellow";
+ctx.arc(100, 145, 40, 0, 2*pi, false);
+ctx.stroke();
+ctx.fill();
+
+
+
+
+//Длинные
+  ctx.strokeStyle = "green";
+    ctx.lineWidth = "5";
+    ctx.moveTo(100,185);
+    ctx.lineTo(100,300);
+    ctx.stroke(); 
+     ctx.beginPath();
+     ctx.beginPath();
+
+
+
+
+
+
+
+ctx.moveTo(100, 225); // координаты начальной точки
+ctx.quadraticCurveTo(75, 200, 25, 190)
+
+
+
+ctx.lineWidth = 4; 
+ctx.strokeStyle="green";
+ctx.fillStyle="green";
+ctx.moveTo(100,225); 
+ctx.quadraticCurveTo(75, 250, 25, 190)
+ctx.stroke();
+ctx.fill();
+
+
+
+ctx.moveTo(100, 250); // координаты начальной точки
+ctx.quadraticCurveTo(150, 250, 300, 125 );
+
+ctx.lineWidth = 4; 
+ctx.strokeStyle="green";
+ctx.fillStyle="green";
+ctx.moveTo(100,250);
+ctx.quadraticCurveTo(100, 350, 300, 125, )
+ctx.stroke();
+ctx.fill();
+
 }
-
-
-function loadStudenst() {
-
-	let table = $('#tbl_all')
-
-	for (let i = 0; i < students.length; i++) {
-		//создадим новую строку
-		let tr = $('<tr>')
-		//создадим 3 ячейки
-		let td1 = $('<td>' + students[i].id + '</td>')
-		let td2 = $('<td>' + students[i].name + '</td>')
-		let td3 = $('<td>' + students[i].surname + '</td>')
-		let td4 = $('<td><button onclick="show_info('+i+')">Подробно</button></td>')
-		
-		//прикрепим ячейки к строке
-		tr.append(td1).append(td2).append(td3).append(td4)
-		//прикрепим строку к таблице
-		table.append(tr)
-	}
-		
-	$('#btn').disabled = true
-}
-
-function show_info(id) {
-	//находим блок
-	let div = $('#info')
-
-	//создадим заголовок
-	let header = $('<h1>Информация о студенте № '+id+'<h1>')
-
-	//вывод имени и фамилии
-	let name = $('<h3>'+students[id].name+' '+students[id].surname+'<h3>')
-	 
-
-	//вывод логотипа
-	let img = $('<img src="http://217.71.129.139:4035/' + students[id].logo+'">')
-
-	//очистим блок
-	div.html('')
-	//добавим элементы к блоку
-	div.append(header).append(name).append(img)
-}
-
