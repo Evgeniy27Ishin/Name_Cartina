929dcb82b7f6d13a915bfc14051ca7e56ffed544
Evgeniy <evgeniy.ishin01@gmail.com>
2022-03-14 16:18:55 +0700

    samostaytel

--- set.js
+++ set.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
-  
-   $("#sort1,#sort2").sortable({connectWith:".connect"});
 
-});
+    $("#el1").draggable({stack:"#el1"}); 
+    $("#el1").resizable();
+    $("#sort1").sortable();
+    $("#el2,#el3").draggable({stack:"#el2,el3"});
+    $("#drop1,#drop2,#el3");
+    $("#drop1").html("Евгений");
+    $("#drop2").html("Ишин")({
+
+	over:function(){
+         $("#warp1").css("background-color","#d7fa99");
+         $("#drop1").css("background-color","#d7fa99");
+      }
+  	});
+
+}); 
