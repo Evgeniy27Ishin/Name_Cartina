0323ced0d1085ac361f33ac9c45e0638e6b4b92a
Evgeniy <evgeniy.ishin01@gmail.com>
2022-03-15 14:55:10 +0700

    3.4

--- set.js
+++ set.js
@@ -1,12 +1,38 @@
-$(document).ready(function(){
-
-   $("#accordion1").accordion();
-   $("#musca").accordion();
-   $("#calicidae").accordion();
-
-
-
-   $("#auto1").autocomplete({source:["Филиппины","Израиль","Германия"]});
+$(document).ready(function() {
    
-   $("#el1,#el2,#el3").button();
+   $("#but1").click(function(){
+      $("#container1").hide($("#effect1").val(),{},1000);
+   });
+   $("#but1").click(function(){
+      $("#container2").hide($("#effect1").val(),{},1000);
+   });
+   $("#but2").click(function(){
+      $("#container1").hide($("#effect1").val(),{},1000);
+   });
+   $("#but2").click(function(){
+      $("#container2").hide($("#effect1").val(),{},1000);
+   });
+   $("#but3").click(function(){
+      $("#container1").hide($("#effect1").val(),{},1000);
+   });
+   $("#but3").click(function(){
+      $("#container2").hide($("#effect1").val(),{},1000);
+   });
+   $("#but4").click(function(){
+      $("#square").animate({
+         borderColor:"#EA3B53",
+         borderWidth:3,
+         backgroundColor:"#97D400",
+         width:500,
+         height:160},1500);
+   });
+   $("#but5").click(function(){
+      $("#square").animate({
+         borderWidth:3,
+         backgroundColor:"#808000",
+         width:500,
+         height:160},1500);
+   });
+   
+
 });
